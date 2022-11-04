b3861f4326e84ba31ce1dc5e26c0553c645cd7e3
MasterMidi <michael-graversen@hotmail.com>
2022-11-04 12:44:52 +0100

    new controller

--- routes/api.php
+++ routes/api.php
@@ -15,31 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/', function (Request $request) {
-    $name = $request->input('name');
-    if (empty($name)) {
-        return 'noname';
-    }
-    return response()->json([
-        'test' => 123,
-        'athhhh' => 'poops',
-        'name' => $name,
-        'time' => time(),
-        'date' => date('Y-m-d H:i:s'),
-        'ip' => $request->ip(),
-        'user-agent' => $request->userAgent(),
-        'server' => $_SERVER,
-        'request' => $request->all(),
-    ]);
-});
-
 //Version control
-Route::prefix('v1')->group(function (){
-    Route::get('/test', function (Request $request) {
-        return "content here";
-    });
+Route::prefix('v1')->group(function () {
+    Route::post('/', [LocationController::class, 'index']);
 });
