59648410c5dfc3a47dd1693ee36e0af63dc2acd8
Michael <work@michael-graversen.dk>
2022-11-22 09:37:57 +0100

    Check cache flow implemented

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\AddressRequest;
+use App\Models\AddressResponse;
+use App\Models\HashRequest;
 use App\Strategies\DenmarkStrategy;
 use App\Strategies\Strategy;
 use App\Strategies\SwedenStrategy;
@@ -21,24 +23,43 @@ class AddressController extends Controller
             'zip_code' => 'required'
         ]);
 
-        $address = new AddressRequest($request->all());
+        // check cache for identical request, else create new instance of hash request class
+        $hash = HashRequest::firstOrNew(
+            ['hash_key' => hash('sha256', json_encode($request->all()))],
+            ['address_id' => null]
+        );
 
-        $strategy = AddressController::getStrategy($address);
+        // verify if there was a record in the database
+        if (isset($hash->id)) {
+            $res = AddressResponse::find($hash->address_id);
+        } else {
 
-        return $strategy->ValidateAddress($address);
+            // create address instance from request information
+            $address = new AddressRequest($request->all());
+            // get the appropriate country strategy and validate the address instance
+            $strategy = AddressController::getStrategy($address);
+            $res = $strategy->ValidateAddress($address);
+
+            // create result on database and set address id on hash request
+            $address_model = AddressResponse::create($res->attributesToArray());
+            $hash->address_id = $address_model->id;
+            $hash->save();
+        }
+
+        return $res;
     }
 
     /**
      * @param AddressRequest $address
-     * @return DenmarkStrategy|SwedenStrategy
+     * @return Strategy
      * @throws Exception
      */
-    private static function getStrategy(AddressRequest $address): SwedenStrategy|DenmarkStrategy
+    private static function getStrategy(AddressRequest $address): Strategy
     {
         return match ($address->country_code) {
             'DK' => new DenmarkStrategy(),
             'SE' => new SwedenStrategy(),
-            default => throw new Exception('Country not supported'),
+            default => throw new Exception('Country not supported'), // or return generic strategy using global services like google-maps
         };
     }
 }
