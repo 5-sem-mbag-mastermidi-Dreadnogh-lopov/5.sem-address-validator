23abf84d6c76dc26d00a9644654e6d5cd537585f
Dreadnogh <56774158+Dreadnogh@users.noreply.github.com>
2022-11-25 13:00:33 +0100

    Chanfges to test client.

--- presentation/src/lib/apiTestTool/ApiTestTool.svelte
+++ presentation/src/lib/apiTestTool/ApiTestTool.svelte
@@ -11,8 +11,6 @@
 
     //TODO: Implement functional to work with API call.
     async function submitRequest() {
-        clearTable();
-
         const obj = address.reduce(
             (obj, item) =>
                 Object.assign(
@@ -26,6 +24,7 @@
 
         if (response.ok) {
             let responseJson = await response.json();
+            responseJson.date = new Date();
             insertAddressTotable(responseJson);
         } else {
             //Toast popup functionality here, no data found.
@@ -34,7 +33,7 @@
     }
 
     function insertAddressTotable(responseJson) {
-        tableData = [...tableData, responseJson];
+        tableData = [responseJson, ...tableData];
     }
 
     function clearTable() {
@@ -48,7 +47,10 @@
         the cache database or can be verified elsewhere
     </p>
 
-    <div class="p-6 flex flex-col items-end">
+    <form
+        class="p-6 flex flex-col items-end"
+        on:submit|preventDefault={submitRequest}
+    >
         {#each address as { name, value }}
             <!-- content here -->
             <div class="flex flex-col p-1">
@@ -63,12 +65,12 @@
                 />
             </div>
         {/each}
-    </div>
-    <button
-        on:click={() => submitRequest()}
-        class="p-2 mt-1 border text-white hover:text-green-500 shadow rounded bg-green-500 m-2  w-44 transition-all hover:scale-105 hover:bg-white hover:border-green-500"
-        >Submit Request</button
-    >
+        <button
+            type="submit"
+            class="p-2 mt-1 border text-white hover:text-green-500 shadow rounded bg-green-500 m-2  w-44 transition-all hover:scale-105 hover:bg-white hover:border-green-500"
+            >Submit Request</button
+        >
+    </form>
 
     <div class="p-12">
         <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
@@ -79,6 +81,7 @@
                     class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
                 >
                     <tr>
+                        <th scope="col" class="py-3 px-6"> Date </th>
                         <th scope="col" class="py-3 px-6"> Confidence </th>
                         <th scope="col" class="py-3 px-6"> Streetname </th>
                         <th scope="col" class="py-3 px-6"> Street Number </th>
@@ -90,20 +93,19 @@
                 </thead>
                 <tbody id="resultTableBody">
                     {#each tableData as item}
-                        <tr
-                            class="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                        >
-                            <td
-                                class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
+                        <tr class="bg-white border-b ">
+                            <td class="py-4 px-6 ">
+                                {item.date.toString().substring(0, 25)}
+                            </td>
+                            <td class="py-4 px-6  font-medium ">
                                 {item["confidence"]}
                             </td>
-                            <td class="py-4 px-6"> {item["street_name"]} </td>
-                            <td class="py-4 px-6"> {item["street_number"]} </td>
-                            <td class="py-4 px-6"> {item["zip_code"]} </td>
-                            <td class="py-4 px-6"> {item["city"]} </td>
-                            <td class="py-4 px-6"> {item["country_name"]} </td>
-                            <td class="py-4 px-6"> {item["country_code"]} </td>
+                            <td class="py-4 px-6"> {item.street_name} </td>
+                            <td class="py-4 px-6"> {item.street_number} </td>
+                            <td class="py-4 px-6"> {item.zip_code} </td>
+                            <td class="py-4 px-6"> {item.city} </td>
+                            <td class="py-4 px-6"> {item.country_name} </td>
+                            <td class="py-4 px-6"> {item.country_code} </td>
                         </tr>
                     {/each}
 
@@ -125,6 +127,13 @@
                     </tr>-->
                 </tbody>
             </table>
+        </div>
+        <div class="flex justify-end p-2 ">
+            <button
+                on:click={clearTable}
+                class="underline hover:text-green-500 transition-all "
+                >Clear</button
+            >
         </div>
     </div>
 </div>
