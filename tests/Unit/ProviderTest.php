7699b3f504315675213bc906b5bf5ba2fabfcd22
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2022-11-29 13:33:34 +0100

    refactor

--- tests/Unit/ProviderTest.php
+++ tests/Unit/ProviderTest.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Integrations\Dawa\DawaProvider;
+use App\Models\AddressRequest;
+
 test('test attributes', function () {
     // Arrange
-    $attr = [
+    $address = new AddressRequest([
         'street' => 'urbansgade 23',
         'state' => '',
         'zip_code'=>'9000',
         'city'=>'Aalborg',
         'country_code' => 'DK'
-    ];
-    $address = new \App\Models\AddressRequest($attr);
-
+    ]);
     // Act
-    $address_attributes = \App\Integrations\Dawa\DawaProvider::get_attributes($address);
+    $address_attributes = DawaProvider::get_attributes($address);
 
     // Assert
     expect($address_attributes)->toEqual('urbansgade 23, 9000 Aalborg');
--- tests/Unit/washingTest.php
+++ tests/Unit/washingTest.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Models\AddressRequest;
 use App\Models\AddressResponse;
 use App\Strategies\Denmark\DenmarkStrategy;
 use Illuminate\Support\Collection;
 
-test('Denmark washing', function () {
+test('Denmark washing - checking object', function () {
 
-    $address = new \App\Models\AddressRequest([
+    $address = new AddressRequest([
         'street' => 'Borg vej  xiiis, 1 tv',
         'zip_code' => '9000',
         'city' => 'Aalborg',
@@ -15,17 +16,27 @@ test('Denmark washing', function () {
 
     $denmark = new DenmarkStrategy();
 
-    $response = $denmark->wash($address);
     $response_type = gettype($denmark->wash($address));
+
+    expect($response_type)->toEqual('object');
+
+});
+
+test('Denmark washing - check if applying washing rules', function () {
+
+    $address = new AddressRequest([
+        'street' => 'Borg vej  xiiis, 1 tv',
+        'zip_code' => '9000',
+        'city' => 'Aalborg',
+        'country_code' => 'DK'
+    ]);
+
+    $denmark = new DenmarkStrategy();
+
     $response_array = $denmark->wash($address)->toArray();
     $response_array_count = count($response_array);
 
-    $validated_address = $denmark->validateAddress($address);
-
-    expect(gettype($validated_address))->toEqual('object');
-
     expect($response_array_count)->toEqual(0 < $response_array_count);
-    expect($response_type)->toEqual('object');
-    expect(gettype($response_array))->toEqual('array');
 
 });
+
